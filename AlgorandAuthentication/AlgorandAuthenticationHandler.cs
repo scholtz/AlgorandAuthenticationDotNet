c122a9d9326104d9365c36e98a2d43fdc8db08e4
Ludovit Scholtz <ludko2@gmail.com>
2023-06-25 16:30:53 +0200

    v 1.0.9, test coverage, msig, rekeying

--- AlgorandAuthentication/AlgorandAuthenticationHandler.cs
+++ AlgorandAuthentication/AlgorandAuthenticationHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Algorand;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Org.BouncyCastle.Crypto.Parameters;
@@ -25,7 +26,7 @@ namespace AlgorandAuthentication
         private readonly ILogger<AlgorandAuthenticationHandler> logger;
         private static DateTimeOffset? t;
         private static ulong block;
-
+        private readonly byte[] EmptySig;
         /// <summary>
         /// Constructor
         /// </summary>
@@ -39,6 +40,7 @@ namespace AlgorandAuthentication
             ISystemClock clock) : base(options, loggerFactory, encoder, clock)
         {
             this.logger = loggerFactory.CreateLogger<AlgorandAuthenticationHandler>();
+            EmptySig = new byte[64];
         }
         /// <summary>
         /// Handle Request
@@ -67,7 +69,17 @@ namespace AlgorandAuthentication
         ///     });
         /// </summary>
         /// <returns></returns>
+        /// 
         protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            return await HandleAuthenticateWithRequestAsync(Request, Options);
+        }
+        /// <summary>
+        /// HandleAuthenticateAsync available for tests
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <returns></returns>
+        public async Task<AuthenticateResult> HandleAuthenticateWithRequestAsync(HttpRequest Request, AlgorandAuthenticationOptions Options)
         {
             try
             {
@@ -79,7 +91,7 @@ namespace AlgorandAuthentication
 
                 if (Options.Debug)
                 {
-                    logger.LogDebug($"Auth header: {Request.Headers[header].ToString()}");
+                    logger.LogDebug($"Auth header: {Request.Headers[header]}");
                 }
                 var auth = Request.Headers[header].ToString();
                 if (!auth.StartsWith(AuthPrefix))
@@ -88,75 +100,25 @@ namespace AlgorandAuthentication
                 }
                 var tx = Convert.FromBase64String(auth.Replace(AuthPrefix, ""));
                 var tr = Algorand.Utils.Encoder.DecodeFromMsgPack<Algorand.Algod.Model.Transactions.SignedTransaction>(tx);
-
-                if (!Verify(tr.Tx.Sender.Bytes, tr.Tx.BytesToSign(), tr.Sig.Bytes))
+                if (tr.Tx == null)
                 {
-                    throw new UnauthorizedException("Signature is invalid");
-                }
-                var networkHash = Convert.ToBase64String(tr.Tx.GenesisHash.Bytes);
-                if (networkHash != Options.NetworkGenesisHash)
-                {
-                    throw new UnauthorizedException($"Invalid Network. Expected {Options.NetworkGenesisHash} received {networkHash}");
-                }
-                if (!string.IsNullOrEmpty(Options.Realm))
-                {
-                    var realm = Encoding.ASCII.GetString(tr.Tx.Note);
-                    if (Options.Realm != realm)
-                    {
-                        // todo: add meaningful message
-                        throw new UnauthorizedException($"Wrong realm. Expected {Options.Realm} received {realm}");
-                    }
-                }
-                DateTimeOffset? expiration = null;
-                if (Options.CheckExpiration)
-                {
-                    ulong estimatedCurrentBlock;
-                    if (t.HasValue && t.Value.AddHours(1) > DateTimeOffset.UtcNow)
-                    {
-                        estimatedCurrentBlock = Convert.ToUInt64((DateTimeOffset.UtcNow - t.Value).TotalSeconds) / 5 + block;
-                    }
-                    else
-                    {
-                        var algodHttpClient = HttpClientConfigurator.ConfigureHttpClient(Options.AlgodServer, Options.AlgodServerToken, Options.AlgodServerHeader);
-                        var algodClient = new Algorand.Algod.DefaultApi(algodHttpClient);
-
-                        var c = await algodClient.GetStatusAsync();
-                        if (c != null)
-                        {
-                            t = DateTimeOffset.UtcNow;
-                            block = (ulong)c.LastRound;
-                        }
-                        estimatedCurrentBlock = block;
-                    }
-
-                    if (tr.Tx.LastValid.Value < estimatedCurrentBlock)
-                    {
-                        throw new UnauthorizedException("Session timed out");
-                    }
-                    expiration = DateTimeOffset.UtcNow + TimeSpan.FromMilliseconds((tr.Tx.LastValid.Value - estimatedCurrentBlock) * Options.MsPerBlock);
+                    throw new UnauthorizedException("Signature is invalid. Does not contain tx.");
                 }
 
-                var user = tr.Tx.Sender.ToString();
-                var claims = new List<Claim>() {
-                    new Claim(ClaimTypes.NameIdentifier,user),
-                    new Claim(ClaimTypes.Name,user),
-                };
 
-                if (Options.CheckExpiration)
+                if (tr.Sig != null)
                 {
-                    if (expiration.HasValue)
-                    {
-                        claims.Add(new Claim("exp", expiration.Value.ToUnixTimeSeconds().ToString()));
-                    }
-                    claims.Add(new Claim("AlgoValidFrom", tr.Tx.FirstValid.Value.ToString()));
-                    claims.Add(new Claim("AlgoValidUntil", tr.Tx.LastValid.Value.ToString()));
+                    return await AlgorandAuthenticationHandler.HandleAuthenticateWithRequestSingleSigAsync(Options, tr);
+                }
+                else if (tr.MSig != null)
+                {
+                    return await HandleAuthenticateWithRequestMultiSigAsync(Options, tr);
+                }
+                else
+                {
+                    throw new UnauthorizedException("Signature is invalid. Does not contain sig or msig.");
                 }
 
-                var identity = new ClaimsIdentity(claims, Scheme.Name);
-                var principal = new ClaimsPrincipal(identity);
-                var ticket = new AuthenticationTicket(principal, Scheme.Name);
-
-                return AuthenticateResult.Success(ticket);
             }
             catch (UnauthorizedException e)
             {
@@ -196,8 +158,147 @@ namespace AlgorandAuthentication
                 return AuthenticateResult.Fail(e);
             }
         }
+        private static async Task<Algorand.Address> AuthAddress(AlgorandAuthenticationOptions Options, Algorand.Algod.Model.Transactions.SignedTransaction tr)
+        {
+            if (string.IsNullOrEmpty(Options.AlgodServer)) // if algod server is not configured do not process rekeying
+            {
+                return tr.Tx.Sender;
+            }
+            var algodHttpClient = HttpClientConfigurator.ConfigureHttpClient(Options.AlgodServer, Options.AlgodServerToken, Options.AlgodServerHeader);
+            var algodClient = new Algorand.Algod.DefaultApi(algodHttpClient);
+            var account = await algodClient.AccountInformationAsync(tr.Tx.Sender.EncodeAsString());
+            return account.AuthAddr ?? tr.Tx.Sender;
+        }
+        /// <summary>
+        /// HandleAuthenticateAsync for single sig tx
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <returns></returns>
+        private static async Task<AuthenticateResult> HandleAuthenticateWithRequestSingleSigAsync(AlgorandAuthenticationOptions Options, Algorand.Algod.Model.Transactions.SignedTransaction tr)
+        {
 
-        private bool Verify(byte[] address, byte[] message, byte[] sig)
+            var sender = await AlgorandAuthenticationHandler.AuthAddress(Options, tr);
+            if (!AlgorandAuthenticationHandler.Verify(sender.Bytes, tr.Tx.BytesToSign(), tr.Sig.Bytes))
+            {
+                throw new UnauthorizedException("Signature is invalid");
+            }
+            return await AlgorandAuthenticationHandler.VerifyCommon(Options, tr);
+        }
+
+        /// <summary>
+        /// HandleAuthenticateAsync for multi sig tx
+        /// </summary>
+        /// <param name="Request"></param>
+        /// <returns></returns>
+        private async Task<AuthenticateResult> HandleAuthenticateWithRequestMultiSigAsync(AlgorandAuthenticationOptions Options, Algorand.Algod.Model.Transactions.SignedTransaction tr)
+        {
+            // validate all signatures
+
+            var checkedSet = new HashSet<string>(); // check if signature is double inserted
+
+            foreach (var subsig in tr.MSig.Subsigs)
+            {
+                var b64 = Convert.ToBase64String(subsig.key.GetEncoded());
+                if (checkedSet.Contains(b64))
+                {
+                    throw new UnauthorizedException("Signature is invalid. Address has been already used.");
+                }
+                checkedSet.Add(b64);
+                if (subsig.sig == null || subsig.sig.Bytes.SequenceEqual(EmptySig))
+                {
+                    continue;
+                }
+                if (!AlgorandAuthenticationHandler.Verify(subsig.key.GetEncoded(), tr.Tx.BytesToSign(), subsig.sig.Bytes))
+                {
+                    throw new UnauthorizedException("Signature is invalid");
+                }
+            }
+            // check if all signators are valid signators
+
+            var multiAddress = new MultisigAddress(tr.MSig.Version, tr.MSig.Threshold, tr.MSig.Subsigs.Select(t => t.key.GetEncoded()).ToList());
+            var sender = await AlgorandAuthenticationHandler.AuthAddress(Options, tr);
+            if (sender.EncodeAsString() != multiAddress.ToAddress().EncodeAsString())
+            {
+                throw new UnauthorizedException("Signature is invalid. Sender of signed transaction is not the multisig object provided");
+            }
+
+            // check if threshold of signatures is ok
+            if (tr.MSig.Threshold < tr.MSig.Subsigs.Where(s => s.sig != null && !s.sig.Bytes.SequenceEqual(EmptySig)).Count())
+            {
+                throw new UnauthorizedException("Signature is invalid. Threshold of signatures has not been met.");
+            }
+
+            return await AlgorandAuthenticationHandler.VerifyCommon(Options, tr);
+        }
+        private static async Task<AuthenticateResult> VerifyCommon(AlgorandAuthenticationOptions Options, Algorand.Algod.Model.Transactions.SignedTransaction tr)
+        {
+
+            var networkHash = Convert.ToBase64String(tr.Tx.GenesisHash.Bytes);
+            if (networkHash != Options.NetworkGenesisHash)
+            {
+                throw new UnauthorizedException($"Invalid Network. Expected {Options.NetworkGenesisHash} received {networkHash}");
+            }
+            if (!string.IsNullOrEmpty(Options.Realm))
+            {
+                var realm = Encoding.ASCII.GetString(tr.Tx.Note);
+                if (Options.Realm != realm)
+                {
+                    // todo: add meaningful message
+                    throw new UnauthorizedException($"Wrong realm. Expected {Options.Realm} received {realm}");
+                }
+            }
+            DateTimeOffset? expiration = null;
+            if (Options.CheckExpiration)
+            {
+                ulong estimatedCurrentBlock;
+                if (t.HasValue && t.Value.AddHours(1) > DateTimeOffset.UtcNow)
+                {
+                    estimatedCurrentBlock = Convert.ToUInt64((DateTimeOffset.UtcNow - t.Value).TotalSeconds) / 5 + block;
+                }
+                else
+                {
+                    var algodHttpClient = HttpClientConfigurator.ConfigureHttpClient(Options.AlgodServer, Options.AlgodServerToken, Options.AlgodServerHeader);
+                    var algodClient = new Algorand.Algod.DefaultApi(algodHttpClient);
+
+                    var c = await algodClient.GetStatusAsync();
+                    if (c != null)
+                    {
+                        t = DateTimeOffset.UtcNow;
+                        block = (ulong)c.LastRound;
+                    }
+                    estimatedCurrentBlock = block;
+                }
+
+                if (tr.Tx.LastValid.Value < estimatedCurrentBlock)
+                {
+                    throw new UnauthorizedException("Session timed out");
+                }
+                expiration = DateTimeOffset.UtcNow + TimeSpan.FromMilliseconds((tr.Tx.LastValid.Value - estimatedCurrentBlock) * Options.MsPerBlock);
+            }
+
+            var user = tr.Tx.Sender.ToString();
+            var claims = new List<Claim>() {
+                    new Claim(ClaimTypes.NameIdentifier,user),
+                    new Claim(ClaimTypes.Name,user),
+                };
+
+            if (Options.CheckExpiration)
+            {
+                if (expiration.HasValue)
+                {
+                    claims.Add(new Claim("exp", expiration.Value.ToUnixTimeSeconds().ToString()));
+                }
+                claims.Add(new Claim("AlgoValidFrom", tr.Tx.FirstValid.Value.ToString()));
+                claims.Add(new Claim("AlgoValidUntil", tr.Tx.LastValid.Value.ToString()));
+            }
+
+            var identity = new ClaimsIdentity(claims, ID);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, ID);
+
+            return AuthenticateResult.Success(ticket);
+        }
+        private static bool Verify(byte[] address, byte[] message, byte[] sig)
         {
 
             var signer = new Ed25519Signer();
---
+++ TestAlgorandAuthentication/MultisigTests.cs
@@ -0,0 +1,108 @@
+using Algorand;
+using Algorand.Algod;
+using Algorand.Algod.Model.Transactions;
+using Algorand.Utils;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using NUnit.Framework.Internal;
+using System.Text.Encodings.Web;
+
+namespace TestAlgorandAuthentication
+{
+    public class Tests
+    {
+        private Algorand.Algod.Model.Account acc1;
+        private Algorand.Algod.Model.Account acc2;
+        private Algorand.Algod.Model.Account acc3;
+        private MultisigAddress multiAddress;
+        private DefaultApi algodApiInstance;
+        private readonly string ALGOD_API_ADDR = "https://testnet-api.algonode.cloud/";
+        private readonly string ALGOD_API_TOKEN = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        private readonly string Realm = "TEST#ARC14";
+        [SetUp]
+        public void Setup()
+        {
+            var httpClient = HttpClientConfigurator.ConfigureHttpClient(ALGOD_API_ADDR, ALGOD_API_TOKEN);
+            algodApiInstance = new DefaultApi(httpClient);
+
+            acc1 = new Algorand.Algod.Model.Account("gravity maid again grass ozone execute exotic vapor fringe snack club monitor where jar pyramid receive tattoo science scene high sound degree bless above good");
+            acc2 = new Algorand.Algod.Model.Account("move sell junior vast verb stove bracket filter place child fame bone story science miss injury put cancel already session cheap furnace void able minimum");
+            acc3 = new Algorand.Algod.Model.Account("pencil ostrich net alpha need vivid elevator gadget bundle meadow flash hamster pig young ten clown before grace arch tennis absent knock peanut ability alarm");
+            multiAddress = new MultisigAddress(1, 2, new List<byte[]> { acc1.Address.Bytes, acc2.Address.Bytes, acc3.Address.Bytes });
+        }
+
+        [Test]
+        public async Task ValidateMultisigTransaction()
+        {
+            var transParams = await algodApiInstance.TransactionParamsAsync();
+            var payment = PaymentTransaction.GetPaymentTransactionFromNetworkTransactionParameters(multiAddress.ToAddress(), multiAddress.ToAddress(), 0, Realm, transParams);
+
+            // sign with 2 addresses (2 of 3 threshold)
+            var signedTx1 = payment.Sign(multiAddress, acc1);
+            var signedTx2 = payment.Sign(multiAddress, acc2);
+            var signedTx = SignedTransaction.MergeMultisigTransactions(signedTx1, signedTx2);
+            var auth = "SigTx " + Convert.ToBase64String(Algorand.Utils.Encoder.EncodeToMsgPackOrdered(signedTx));
+
+            var au = new AlgorandAuthentication.AlgorandAuthenticationOptions() { Debug = true, AlgodServer = ALGOD_API_ADDR, AlgodServerToken = ALGOD_API_TOKEN, Realm = Realm, CheckExpiration = false, NetworkGenesisHash = "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=" };
+            var monitor = Mock.Of<IOptionsMonitor<AlgorandAuthentication.AlgorandAuthenticationOptions>>(_ => _.CurrentValue == au);
+
+            var loggerFactory = LoggerFactory.Create(c => c.AddConsole().SetMinimumLevel(LogLevel.Debug));
+
+            var urlEncoder = UrlEncoder.Default;
+            var clock = new RealSystemClock();
+            var handler = new AlgorandAuthentication.AlgorandAuthenticationHandler(monitor, loggerFactory, urlEncoder, clock);
+
+            var context = new DefaultHttpContext();
+            context.Request.Headers.Authorization = auth;
+
+            var result = await handler.HandleAuthenticateWithRequestAsync(context.Request, au);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Succeeded, Is.True);
+        }
+
+        [Test]
+        public async Task ValidateSinglesigTransaction()
+        {
+            var transParams = await algodApiInstance.TransactionParamsAsync();
+            var payment = PaymentTransaction.GetPaymentTransactionFromNetworkTransactionParameters(acc1.Address, multiAddress.ToAddress(), 0, Realm, transParams);
+            var signedTx = payment.Sign(acc2); // rekeyd
+            var auth = "SigTx " + Convert.ToBase64String(Algorand.Utils.Encoder.EncodeToMsgPackOrdered(signedTx));
+
+            var au = new AlgorandAuthentication.AlgorandAuthenticationOptions() { Debug = true, AlgodServer = ALGOD_API_ADDR, AlgodServerToken = ALGOD_API_TOKEN, Realm = Realm, CheckExpiration = false, NetworkGenesisHash = "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=" };
+            var monitor = Mock.Of<IOptionsMonitor<AlgorandAuthentication.AlgorandAuthenticationOptions>>(_ => _.CurrentValue == au);
+
+            var loggerFactory = LoggerFactory.Create(c => c.AddConsole().SetMinimumLevel(LogLevel.Debug));
+
+            var urlEncoder = UrlEncoder.Default;
+            var clock = new RealSystemClock();
+            var handler = new AlgorandAuthentication.AlgorandAuthenticationHandler(monitor, loggerFactory, urlEncoder, clock);
+
+            var context = new DefaultHttpContext();
+            context.Request.Headers.Authorization = auth;
+
+            var result = await handler.HandleAuthenticateWithRequestAsync(context.Request, au);
+
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result.Succeeded, Is.True);
+        }
+    }
+
+    public class RealSystemClock : Microsoft.AspNetCore.Authentication.ISystemClock
+    {
+        public DateTimeOffset UtcNow => DateTimeOffset.UtcNow;
+    }
+    class Events
+    {
+
+    }
+    class EventsType
+    {
+
+    }
+
+}
